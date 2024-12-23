9f6f441f1a8d4341b7809acf284775f197d0f87a
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-12-23 12:34:58 -0600

    Login Form update

--- app/src/main/java/com/carlostorres/pruebatecnicagonet/login/ui/LoginScreen.kt
+++ app/src/main/java/com/carlostorres/pruebatecnicagonet/login/ui/LoginScreen.kt
@@ -2,13 +2,17 @@ package com.carlostorres.pruebatecnicagonet.login.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -21,6 +25,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
@@ -233,6 +238,21 @@ fun LoginScreen(
                             viewModel.onEvent(LoginEvents.Login)
                         }
                     )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = "Don't have an account?")
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(
+                            text = "Sign Up",
+                            color = BgPurple,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
 
                 }
             }
