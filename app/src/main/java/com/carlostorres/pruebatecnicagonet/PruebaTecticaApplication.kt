5d95cfe519030815ebd42287cfe1d2aa9efd3ca9
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-12-22 19:17:11 -0600

    Init Config

---
+++ app/src/main/java/com/carlostorres/pruebatecnicagonet/PruebaTecticaApplication.kt
@@ -0,0 +1,7 @@
+package com.carlostorres.pruebatecnicagonet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PruebaTecticaApplication : Application()
---
+++ app/src/main/java/com/carlostorres/pruebatecnicagonet/ui/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.carlostorres.pruebatecnicagonet.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.carlostorres.pruebatecnicagonet.ui.theme.PruebaTecnicaGonetTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaTecnicaGonetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Carlos",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaTecnicaGonetTheme {
+        Greeting("Android")
+    }
+}
